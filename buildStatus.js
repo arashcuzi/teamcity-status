26a9138d7d579f3f49badc65bce35f221b4709e3
Shawn Petros <arashcuzi@gmail.com>
2016-12-29 13:43:21 -0600

    basically rewrote the whole thing...

--- buildStatus.js
+++
@@ -1,5 +0,0 @@
-var Enum = require('enum');
-
-var status = new Enum(['BUILDING', 'SUCCESS', 'FAILURE', 'UNKNOWN'], 'status')
-
-exports.status = status;
--- server.js
+++ server.js
@@ -1,28 +1,97 @@
 var request = require('request');
-var ConfigurationCollection = require('./ConfigurationCollection');
-var TeamCityStatusChecker = require('./TeamCityStatusChecker');
-var DelcomIndicator = require('delcom-indicator');
-var config = require('./config.json');
+// var ConfigurationCollection = require('./ConfigurationCollection');
+// var TeamCityStatusChecker = require('./TeamCityStatusChecker');
+var blinkstick = require('blinkstick');
+// var config = require('./config.json');
 
-var pollInterval = 10 * 1000;
-var lightDelay = 5 * 1000;
+// var pollInterval = 10 * 1000;
+// var lightDelay = 5 * 1000;
+//
+var led = blinkstick.findFirst();
 
-var delcomIndicator = new DelcomIndicator();
-delcomIndicator.turnOff();
+led.setColor('blue');
 
-var configurations = new ConfigurationCollection(config, new TeamCityStatusChecker(), delcomIndicator);
-checkStatusAndSetLight();
+var currentStatus = 'normal';
+var lastStatus = 'normal';
+//
+// var configurations = new ConfigurationCollection(config, new TeamCityStatusChecker(), led);
+// checkStatusAndSetLight();
+//
+// setInterval(function(){
+//     checkStatusAndSetLight();
+// }, pollInterval);
+//
+// function checkStatusAndSetLight(){
+//     configurations.checkStatus();
+//     setInterval(function(){
+//         configurations.displayStatus();
+//     }, lightDelay);
+// }
+var options = {
+  url: 'http://teamcity.nml.com/httpAuth/app/rest/cctray/projects.xml',
+  username: 'pet7915',
+  password: 'CVP12345'
+};
 
-setInterval(function(){
-    checkStatusAndSetLight();
-}, pollInterval);
+function checkStatus() {
+	request.get(
+		options.url,
+		{
+			auth: {
+				user: options.username,
+				pass: options.password,
+				sendImmediately: true,
+			},
+			json: true
+		},
+		function(err, resp, body) {
+			// console.log(err);
+			const building = body.Project.filter(p => {
+				return p.activity === 'Building';
+			});
 
-function checkStatusAndSetLight(){
-    configurations.checkStatus();
-    setInterval(function(){
-        configurations.displayStatus();
-    }, lightDelay);
+			const failed = body.Project.filter(p => {
+				return p.lastBuildStatus === 'Failure';
+			});
+
+			// console.log(building.length, failed.length, body.Project);
+
+			if (building.length > 0) {
+				setLights('Building');
+			} else if (failed.length > 0) {
+				setLights('Failure');
+			} else {
+				setLights('Success');
+			}
+		}
+	);
 }
 
+function setLights(status) {
+	switch (status) {
+		case 'Building':
+			led.blink('purple');
+			// console.log('building');
+			break;
+		case 'Success':
+			led.setColor('green');
+			// console.log('success');
+			break;
+		case 'Failure':
+			led.pulse('red', function() {
+				led.pulse('red', function() {
+					led.pulse('red', function() {
+						led.setColor('red');
+					});
+				});
+			});
+			// console.log('failed');
+			break;
+		default:
+			led.setColor('blue');
+	}
+}
 
-
+setInterval(function() {
+	checkStatus();
+}, 10000);
