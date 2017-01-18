5a6cf04055c8d33acf13e1938c31a55edddd1679
Shawn Petros <arashcuzi@gmail.com>
2017-01-18 13:10:18 -0600

    changed tabs to spaces for readability

--- server.js
+++ server.js
@@ -12,73 +12,73 @@ var currentStatus = 'normal';
 var lastStatus = 'normal';
 
 function checkStatus() {
-	request.get(
-		conf.url,
-		{
-			auth: {
-				user: conf.user,
-				pass: conf.pass,
-				sendImmediately: true,
-			},
-			json: true
-		},
-		function(err, resp, body) {
-			if (err) return err;
+  request.get(
+    conf.url,
+    {
+      auth: {
+        user: conf.user,
+        pass: conf.pass,
+        sendImmediately: true
+      },
+      json: true
+    },
+    function(err, resp, body) {
+      if (err) return err;
 
-			setStatus({ Project: [body.Project[0]] });
-		}
-	);
+      setStatus({ Project: [body.Project[0]] });
+    }
+  );
 }
 
 function setStatus(body) {
-	const building = getBuilds(body);
-	const failed = getFailed(body);
-	const build = building.length > 0 ? true : false;
-	const fail = failed.length > 0 ? true : false;
+  const building = getBuilds(body);
+  const failed = getFailed(body);
+  const build = building.length > 0 ? true : false;
+  const fail = failed.length > 0 ? true : false;
 
-	const status = getCurrentStatus(build, fail);
-	setCurrentStatus(status);
+  const status = getCurrentStatus(build, fail);
+  setCurrentStatus(status);
 
-	if (build) {
-		setLights('Building');
-	} else if (fail && currentStatus === lastStatus) {
-		setLights('Still broke');
-	} else if (fail) {
-		setLights('Failure');
-	} else {
-		setLights('Success');
-	}
+  if (build) {
+    setLights('Building');
+  } else if (fail && currentStatus === lastStatus) {
+    setLights('Still broke');
+  } else if (fail) {
+    setLights('Failure');
+  } else {
+    setLights('Success');
+  }
 }
 
 function getCurrentStatus(b, f) {
-	if (b) return 'Building';
-	if (f) return 'Failure';
+  if (b) return 'Building';
+  if (f) return 'Failure';
 
-	if (!b && !f) return 'Success';
+  if (!b && !f) return 'Success';
 }
 
 function setCurrentStatus(newStatus) {
-	lastStatus = currentStatus;
-	currentStatus = newStatus;
+  lastStatus = currentStatus;
+  currentStatus = newStatus;
 }
 
 function getBuilds(body) {
-	return body.Project.filter(p => p.activity === 'Building');
+  return body.Project.filter(p => p.activity === 'Building');
 }
 
 function getFailed(body) {
-	return body.Project.filter(p => p.lastBuildStatus === 'Failure');
+  return body.Project.filter(p => p.lastBuildStatus === 'Failure');
 }
 
 function blink(color) {
-	led.setColor(color);
+  led.setColor(color);
 
-	ledTimeout = setTimeout(function() {
-		led.setColor(0, 0, 0);
-		ledTimeout = setTimeout(function() {
-			blink(color);
-		}, 1000);
-	}, 1000);
+  ledTimeout = setTimeout(function() {
+    led.setColor(0, 0, 0);
+    ledTimeout = setTimeout(function() {
+      blink(color);
+    }, 1000);
+  }, 1000);
 }
 
 function stop() {
@@ -87,27 +87,27 @@ function stop() {
 }
 
 function setLights(status) {
-	let now = new Date();
-	console.log(`${now.toUTCString()} status:`, status);
-	stop();
-	switch (status) {
-		case 'Building':
-			blink('purple');
-			break;
-		case 'Success':
-			led.setColor('green');
-			break;
-		case 'Failure':
-		case 'Still broke':
-			blink('red');
-			break;
-		default:
-			led.setColor('blue');
-	}
+  let now = new Date();
+  console.log(`${now.toUTCString()} status:`, status);
+  stop();
+  switch (status) {
+    case 'Building':
+      blink('purple');
+      break;
+    case 'Success':
+      led.setColor('green');
+      break;
+    case 'Failure':
+    case 'Still broke':
+      blink('red');
+      break;
+    default:
+      led.setColor('blue');
+  }
 }
 
 checkStatus();
 
 setInterval(function() {
-	checkStatus();
+  checkStatus();
 }, 10000);
